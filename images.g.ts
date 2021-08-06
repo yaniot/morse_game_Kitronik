99ebe11461c78c18d79026bb5b2b8e234171b85d
@yanqisui <59643508+yaniot@users.noreply.github.com>
2021-08-06 21:06:34 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,19 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
